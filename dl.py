642657e49559b59e7a121ab67fe24d6c38c34f4d
Jasperaj <46399293+Jasperaj@users.noreply.github.com>
2022-03-05 14:36:51 +0545

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,11 +1,12 @@
 import sys, os
 from skillshare import Skillshare, splash
+from magic import cookie
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
 
 def main():
-    dl = Skillshare("PHPSESSID=407aa237fa31508a488f477c7f420358")
+    dl = Skillshare(cookies)
     course_url = "https://www.skillshare.com/classes/The-Art-of-Company-Valuation-Advanced-course/1780499879"
     dl.download_course_by_url(course_url)
 
